a6499569069925b437de42760010dbc55787cea6
subhonita-sarkar <subhonita0809@gmail.com>
2026-06-07 10:52:19 +0530

    analyze endpoint added

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 app = FastAPI()
+
+class CodeInput(BaseModel):
+    code: str
+    language: str = "python"
+
 @app.get("/")
 def home():
-    return {"message": "Hello World"}
+    return {"message": "Code Complexity Analyzer working!"}
+
+@app.post("/analyze")
+def analyze_code(input: CodeInput):
+    return{
+        "code_recieved": input.code,
+        "language": input.language,
+        "message": "Analysis coming soon!"
+    }
